0279a748469aeef62de93e18e0d93ffe56672573
zcrosman <zcrosman@users.noreply.github.com>
2024-05-28 14:34:05 -0500

    added recursive searches

--- gitemails.py
+++ gitemails.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import warnings
 
 # Suppress the insecure request warnings (requests verify=False)
-# Used for testing self hosted git labs that could be self signed
+# Used for testing self-hosted git labs that could be self-signed
 warnings.filterwarnings('ignore', category=requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 def token_generator(token_list):
@@ -31,20 +31,13 @@ def get_api_response(url, token_gen=None):
             return response
         elif response.status_code == 403 and 'X-RateLimit-Reset' in response.headers and response.headers['X-RateLimit-Remaining'] == '0':
             print("\nRate limit exceeded!!!")
-            # retry_after = datetime.utcfromtimestamp(int(response.headers.get('X-Ratelimit-Reset')))
-            # print(f'Github - Retry after time {str(retry_after)}')
-            # current_time = datetime.now()
-            # print(f'Current time {str(current_time)}')
-            # wait_seconds = (retry_after - current_time).total_seconds() + 10
-            # print(f'Wait seconds {str(int(wait_seconds))}')
-
             if token_gen:
                 print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Rate limit exceeded. Switching token and retrying after 60 seconds.")
                 print("\nRetrying in 60 seconds.")
                 time.sleep(60)
                 continue
             else:
-                print("\nIt is highly reccomended to use github tokens if you are scanning large targets")
+                print("\nIt is highly recommended to use GitHub tokens if you are scanning large targets")
                 print("     Tokens can be generated at https://github.com/settings/tokens\n")
                 print("\nRetrying in 60 seconds.")
                 time.sleep(60)
@@ -105,6 +98,7 @@ def get_repositories_for_org(orgname, token_gen=None):
 def get_emails_from_github_commits(repo, csv_writer, token_gen=None):
     url = f"https://api.github.com/repos/{repo['owner']}/{repo['name']}/commits"
     repo_emails = set()
+    commit_usernames = []
 
     while url:
         print(f'Searching Project Commits ({repo["name"]}): {url}')
@@ -114,26 +108,36 @@ def get_emails_from_github_commits(repo, csv_writer, token_gen=None):
         for commit in commits:
             try:
                 author_email = commit['commit']['author']['email']
-                author_username = commit['commit']['author']['name']
+                author_name = commit['commit']['author']['name']
+                try:
+                    author_gh_username = commit['author']['login']
+                    commit_usernames.append(author_gh_username)
+                except:
+                    author_gh_username = 'Not Found'
                 committer_email = commit['commit']['committer']['email']
                 committer_username = commit['commit']['committer']['name']
-                email_author_pair = (author_email, author_username)
-                email_commiter_pair = (committer_email, committer_username)
+                email_author_pair = (author_gh_username, author_email, author_name)
+                email_committer_pair = (committer_username, committer_email, committer_username)
                 commit_url = commit['html_url']
                 commit_api = url
 
                 if 'noreply' not in author_email and email_author_pair not in repo_emails:
                     repo_emails.add(email_author_pair)
-                    role = "Owner" if author_username.lower() == repo['owner'].lower() else "Contributor"
-                    csv_writer.writerow([repo['name'], repo['url'], repo['owner'], author_username, role, 'Commit Author', author_email, commit_url, commit_api])
-                    print(f"    author email logged - {author_email}")
-                if 'noreply' not in committer_email and email_commiter_pair not in repo_emails:
-                    repo_emails.add(email_commiter_pair)
+                    role = "Owner" if author_gh_username.lower() == repo['owner'].lower() else "Contributor"
+                    csv_writer.writerow([repo['name'], repo['url'], repo['owner'], author_gh_username, author_name, role, 'Commit Author', author_email, commit_url, commit_api])
+                    unique_combos.add(email_author_pair)
+                    print(f"    Author email logged - {author_email}")
+                
+                if 'noreply' not in committer_email and email_committer_pair not in repo_emails:
+                    repo_emails.add(email_committer_pair)
                     role = "Owner" if committer_username.lower() == repo['owner'].lower() else "Contributor"
-                    csv_writer.writerow([repo['name'], repo['url'], repo['owner'], committer_username, role, 'Commit Committer', committer_email, commit_url, commit_api])
-                    print(f"    committer email logged - {committer_email}")
+                    csv_writer.writerow([repo['name'], repo['url'], repo['owner'], committer_username, committer_username, role, 'Commit Committer', committer_email, commit_url, commit_api])
+                    unique_combos.add(email_committer_pair)
+                    print(f"    Committer email logged - {committer_email}")
 
             except Exception as e:
+                print(url)
+                print(commit)
                 print("Error processing commit:", e)
 
         if 'next' in response.links:
@@ -141,9 +145,27 @@ def get_emails_from_github_commits(repo, csv_writer, token_gen=None):
         else:
             break
 
+    return commit_usernames
+
+def recursive_search(usernames, depth, token_gen, csv_writer, current_depth=0):
+    if current_depth > depth:
+        return
+
+    new_usernames = set()
+    total_users = len(usernames)
+    
+    for i, username in enumerate(usernames):
+        print(f"Fetching repositories for user: {username} ({i+1}/{total_users})")
+        repositories = get_repositories_for_user(username, token_gen)
+        for repo in repositories:
+            commit_usernames = get_emails_from_github_commits(repo, csv_writer, token_gen)
+            new_usernames.update(commit_usernames)
+
+    recursive_search(new_usernames, depth, token_gen, csv_writer, current_depth + 1)
+
 def main():
     parser = argparse.ArgumentParser(
-        description='This script fetches GitHub repositories and commit emails for a specified user or organization.\nGithub:zcrosman :)',
+        description='This script fetches GitHub repositories and commit emails for a specified user or organization.\nGitHub: zcrosman :)',
         epilog='''Example usage:
     python gitemails.py --user john_doe 
     python gitemails.py --user john_doe --token YOUR_GITHUB_TOKEN
@@ -158,6 +180,7 @@ def main():
     group_user_org.add_argument('--org', type=str, help='GitHub organization to fetch repositories for')
     group_token.add_argument('--token', type=str, help='GitHub token')
     group_token.add_argument('--token-file', type=str, help='File containing GitHub token(s)')
+    parser.add_argument('--depth', type=int, default=0, help='Depth for recursive search of commit usernames')
 
     args = parser.parse_args()
 
@@ -172,19 +195,49 @@ def main():
         token_gen = token_generator(tokens)
     
     repositories = []
+    initial_usernames = []
+    global unique_combos
+    unique_combos = set()
+
     if args.user:
         print(f"Fetching repositories for user: {args.user}")
         repositories = get_repositories_for_user(args.user, token_gen)
+        initial_usernames.append(args.user)
     elif args.org:
         print(f"Fetching repositories for organization: {args.org}")
         repositories = get_repositories_for_org(args.org, token_gen)
+        org_users = set()
+        with open(f'github-data-{args.org}.csv', 'a', newline='', encoding='utf-8') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(['Repo Name', 'Repo URL', 'Repo Owner', 'GH Username', 'Name', 'Role', 'Type', 'Email', 'Commit URL', 'Commit API URL'])
+            for repo in repositories:
+                commit_usernames = get_emails_from_github_commits(repo, csv_writer, token_gen)
+                org_users.update(commit_usernames)
+        initial_usernames = list(org_users)
 
     filename = f'github-data-{args.user or args.org}.csv'
+    commit_usernames = []
     with open(filename, 'a', newline='', encoding='utf-8') as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(['Repo Name', 'Repo URL', 'Repo Owner', 'Username', 'Role', 'Type', 'Email', 'Commit URL', 'Commit API URL'])
+        csv_writer.writerow(['Repo Name', 'Repo URL', 'Repo Owner', 'GH Username', 'Name', 'Role', 'Type', 'Email', 'Commit URL', 'Commit API URL'])
         for repo in repositories:
-            get_emails_from_github_commits(repo, csv_writer, token_gen)
+            commit_usernames.extend(get_emails_from_github_commits(repo, csv_writer, token_gen))
+
+        unique_usernames = list(set(commit_usernames))
+        print("\nUnique commit usernames:")
+        for username in unique_usernames:
+            print(username)
+
+        if args.depth > 0:
+            recursive_search(initial_usernames, args.depth, token_gen, csv_writer)
+
+    # Write unique combinations to a separate CSV file
+    unique_combos_filename = f'unique-combos-{args.user or args.org}.csv'
+    with open(unique_combos_filename, 'w', newline='', encoding='utf-8') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['GH Username', 'Email', 'Name'])
+        for combo in unique_combos:
+            csv_writer.writerow(combo)
 
 if __name__ == "__main__":
     main()
